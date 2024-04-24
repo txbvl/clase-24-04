183c32609a27c6e8690e1b84aa0af0a8146bc30d
unknown <SSDD@PNAWSA06LC1010.duoc.acad>
2024-04-24 15:14:15 -0400

    actualizacion 2

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+        $("h1").click(function(){
+            $(this).text("Somos el mejor pais de chile hermano");
+        });
+
+});
+
+    let idChile = document.getElementById("idChile");
+    idChile.addEventListener("click", function(){
+        idChile.innerHTML = ""
+
+    })
