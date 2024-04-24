66026f304038d6789f29a418310231046c655fb8
unknown <SSDD@PNAWSA06LC1010.duoc.acad>
2024-04-24 15:57:27 -0400

    Actualizacion 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,51 @@
 $(document).ready(function(){
+        let num1 = 0;
+        let num2 = 0;
+        let resultado = 0;
+
+        let error1 = $("#error1").
+
+        $("#resultado").css("color","red");
+        $("#resultado").css("font-size","1.2rem");
+        $("#resultado").hide();
+
         $("h1").click(function(){
             $(this).text("Somos el mejor pais de chile hermano");
         });
 
+        $("#btnSumar").click(function(){
+
+            $("#error1").text("");
+            $("#error2").text("");   
+
+
+            if($("#numero1").val() === ""){
+                $("#error1").text("Debe agregar el primer numero");
+                $("#error1").css("color","red");
+                return;
+            }
+            if($("#numero2").val() === ""){
+                $("#error2").text("Debe agregar el segundo numero");
+                $("#error2").css("color","red");
+                return;
+            }
+
+            num1 = parseInt($("#numero1").val());
+            num2 = parseInt($("#numero1").val());
+            resultado = num1 + num2; 
+
+            $("#resultado").show();
+            $("#resultado").text(resultado);
+        });
 });
 
-    let idChile = document.getElementById("idChile");
-    idChile.addEventListener("click", function(){
-        idChile.innerHTML = ""
+//    let idChile = document.getElementById("idChile");
+//    idChile.addEventListener("click", function(){
+//        idChile.innerHTML = "No tengo porque estar de acuerdo con lo que pienso";
 
-    })
+//    })
+
+//    let btnSumar = document.getElementById("btnSumar");
+//    btnSumar.addEventListener("click", (e) => {
+
+//    })
